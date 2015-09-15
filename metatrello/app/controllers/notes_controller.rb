856bdb59b34346b1aecec76207901696b8cee7d2
kennygeiler <kennethlgeiler@gmail.com>
2015-09-14 23:49:34 -0500

    add create method functionality and private params method

--- metatrello/app/controllers/notes_controller.rb
+++ metatrello/app/controllers/notes_controller.rb
@@ -1,11 +1,23 @@
 class NotesController < ApplicationController
 
   def new
-
+    @note = Note.new
   end
 
   def create
+    @note = Note.create(url_params)
+    if @note.save
+      redirect_to url_notes_path(@note.url_id)
+    else
+      render new_url_note_path(@note.url_id)
+    end
 
+  end
+
+  private
+
+  def notes_params
+    params.require(:note).permit(:url_id, :title, :body)
   end
 
 
