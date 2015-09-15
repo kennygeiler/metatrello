a33ba7da65bec4dfec92127653811983a29cba78
kennygeiler <kennethlgeiler@gmail.com>
2015-09-14 23:40:20 -0500

    fix errors in url controller

--- metatrello/app/controllers/urls_controller.rb
+++ metatrello/app/controllers/urls_controller.rb
@@ -1,10 +1,11 @@
 class UrlsController < ApplicationController
 
   def new
+    @url = Url.new
   end
 
   def create
-    @url = Url.create(params[:url])
+    @url = Url.create(url_params)
     if @url.save
       redirect_to @url
     else
@@ -12,5 +13,10 @@ class UrlsController < ApplicationController
     end
   end
 
+private
+
+ def url_params
+    params.require(:url).permit(:name, :private, :password)
+  end
 
 end
