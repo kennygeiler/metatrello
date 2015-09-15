e1f877e3047c0ce27d60cc819d8a37716c6fa419
kennygeiler <kennethlgeiler@gmail.com>
2015-09-14 23:17:56 -0500

    update notes table

--- metatrello/db/migrate/20150915041011_create_notes.rb
+++ metatrello/db/migrate/20150915041011_create_notes.rb
@@ -1,7 +1,7 @@
 class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
-      t.integer :url_id
+      t.references :url
       t.string :title
       t.text :body
       t.timestamps null: false
--- metatrello/db/schema.rb
+++ metatrello/db/schema.rb
@@ -14,6 +14,9 @@
 ActiveRecord::Schema.define(version: 20150915041011) do
 
   create_table "notes", force: :cascade do |t|
+    t.integer  "url_id"
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
