264ff0ce5a3c26ce5188e4c43272704f46b073ac
kennygeiler <kennethlgeiler@gmail.com>
2015-09-14 23:23:59 -0500

    add new and create method in url controller

--- metatrello/app/controllers/urls_controller.rb
+++ metatrello/app/controllers/urls_controller.rb
@@ -1,2 +1,17 @@
 class UrlsController < ApplicationController
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.create(params[:url])
+    if @url.save
+      redirect_to @url
+    else
+      render "new"
+    end
+  end
+
+
 end
